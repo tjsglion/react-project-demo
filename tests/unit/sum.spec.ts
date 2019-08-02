00490a50e51e5b3dd3fc7ef23312c14d774df7d2
tjsglion <jiang.tang@aliyun.com>
2019-08-02 18:07:35 +0800

    优化pagckage.json文件

--- tests/unit/sum.spec.ts
+++ tests/unit/sum.spec.ts
@@ -1,5 +1,5 @@
-// import sum from '../../src/web/components/test';
+import sum from '../../src/web/components/test';
 
-// test('sum', () => {
-//   expect(sum(1, 2)).toBe(3);
-// })
+test('sum', () => {
+  expect(sum(1, 2)).toBe(3);
+})
