9c77a40160e6225c491d00dae17cac2e874a114c
tjsglion <jiang.tang@aliyun.com>
2019-08-07 00:10:48 +0800

    routes配置

--- src/web/routers/index.tsx
+++ src/web/routers/index.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react';
-import {Route, Switch, RouteProps, Redirect} from 'react-router-dom';
+import { Route, Switch, RouteProps, Redirect } from 'react-router-dom';
 const { Suspense, lazy } = React;
 // import Index from '@components/Index';
-const Index = lazy(() => import(/* webpackChunkName: "index" */ "@components/Index"))
+const Index = lazy(() =>
+  import(/* webpackChunkName: "index" */ '@components/Index')
+);
+
+// 定义路由属性接口
+interface Props extends RouteProps {
+  auth?: boolean;
+}
+
+const routes: Props[] = [
+  {
+    path: '/',
+    exact: true,
+    auth: false,
+    component: Index
+  }
+];
+
+console.log(Index);
